6c84ef09f4e071fdf603af66fe410e6f3bb4eec5
wei3511@126.com <wei3511@126.com>
2018-03-24 10:50:37 -0700

    添加ueditor_demo示例项目

---
+++ Ueditor_demo/Ueditor_demo.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+from ueditor import bp
+import config
+
+app = Flask(__name__)
+app.config.from_object(config)
+app.register_blueprint(bp)
+
+@app.route('/')
+def hello_world():
+	return render_template('index.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True,port=1000)
---
+++ Ueditor_demo/config.py
@@ -0,0 +1,11 @@
+import os
+#本地存储图片地址
+UEDITOR_UPLOAD_PATH = os.path.join(os.path.dirname(__file__),'images')
+
+#填写七牛的配置信息后 就会直接将图片上传值七牛云上！！UEDITOR_UPLOAD_TO_QINIU=True
+# UEDITOR_UPLOAD_TO_QINIU = False
+# UEDITOR_QINIU_ACCESS_KEY = ""
+# UEDITOR_QINIU_SECRET_KEY = ""
+# UEDITOR_QINIU_BUCKET_NAME = ""
+# UEDITOR_QINIU_DOMAIN = ""
+
