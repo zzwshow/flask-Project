790efcb1d9708787041db9e249f1214b862fbd8e
wei3511@126.com <wei3511@126.com>
2018-03-24 07:21:07 -0700

    版块管理编辑功能完成

--- xmbbs/static/cms/js/boards.js
+++ xmbbs/static/cms/js/boards.js
@@ -1,4 +1,4 @@
-
+//监听添加新版块按钮
 $(function () {
    $('#add-board-btn').click(function (event) {
        event.preventDefault();
@@ -15,7 +15,6 @@ $(function () {
                    },
                    'success':function (data) {
                        if (data['code']===200){
-                           zlalert.alertSuccess(data['message']);
                            window.location.reload();
                        }else {
                            zlalert.alertInfo(data['message']);
@@ -26,6 +25,44 @@ $(function () {
        });
    });
 });
+
+//监听编辑按钮
+$(function () {
+    $(".edit-board-btn").click(function (event) {
+        event.preventDefault();
+        var self = $(this);
+        var tr = self.parent().parent();  //找到编辑按钮的两层父及标签tr
+
+        var name = tr.attr('data-name');
+        var board_id = tr.attr('data-id');
+            
+            //zlajax中的弹出框
+            zlalert.alertOneInput({
+                'title':'编辑版块',
+                'text':'请输入新的版块名称',
+                'placeholder':name,
+                'confirmText':'确认',
+                'confirmCallback':function (inputValue) {
+                    zlajax.post({
+                        'url':"/cms/uboard/",
+                        'data':{
+                            'board_id':board_id,
+                            'name':inputValue
+                        },
+                        'success':function (data) {
+                            if(data['code']===200){
+                                window.location.reload();
+                            }else {
+                                zlalert.alertInfo(data['message']);
+                            }
+                        }
+                    });
+                }
+            });
+
+    });
+});
+
 
 
 
